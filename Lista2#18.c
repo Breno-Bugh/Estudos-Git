19a2410fd61d77543e96c8ff243b5e72873502b4
Breno-Bugh <brenobugh.queiroz@gmail.com>
2026-09-03 20:12:04 -0300

    exercicio 18 lista 2

--- Lista2#18.c
+++ Lista2#18.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Olá, mundo!\n");
+    double salario, bonus;
+    int anos;
+    printf("Digite seu salario: ");
+    scanf("%lf", &salario);
+    printf("Digite ha quantos anos trabalha na empresa: ");
+    scanf("%d", &anos);
+    if(anos>=5){
+        bonus=salario*0.2;
+    }else{
+        bonus=salario*0.1;
+    }
+    printf("Seu bonus salarial e igual a: R$%.2lf", bonus);
     return 0;
 }
